abfafad5ad069c2ce26fc5b745ba821d74a20aef
cyanyaan <ankitsarkar431@proton.me>
2025-01-25 12:04:42 +0530

    kekw

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-@app.route('/sex')
 def index():
     user = {'username' : 'Ankit'}
     posts = [
@@ -17,3 +17,8 @@ def index():
         }
     ]
     return render_template('index.html', title= 'Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template("login.html", title = "Sign in", form = form)
